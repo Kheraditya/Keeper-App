c85c73d5912673c658d86feccdcd3e02b2a23ddb
Adtiya Kher <adityakher303@gmail.com>
2023-04-01 22:44:08 +0530

    complete keeper app

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+function Header() {
+  return (
+    <header>
+      <h1>
+      <FontAwesomeIcon icon="fa-regular fa-file-shield" />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
